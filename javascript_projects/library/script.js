ad4902ed04289481e992f15cd6fe57b7f46045fc
Nipun Tewari <nipuntewari11@gmail.com>
2021-06-07 16:14:26 +0530

    figure our JS

--- javascript_projects/library/script.js
+++ javascript_projects/library/script.js
@@ -1,7 +1,6 @@
-function bookCreator(name, author, pages, isRead) {
+function bookCreator(name, author, isRead) {
   this.name = name;
   this.author = author;
-  this.pages = pages;
   this.isRead = isRead;
 }
 
@@ -13,9 +12,25 @@ addBookButton.addEventListener("click", openForm);
 let close = document.querySelector(".btn.cancel");
 close.addEventListener("click", closeForm);
 
+let submit = document.querySelector("#submitButton");
+submit.addEventListener("click", updateLibrary);
+
 function openForm() {
   document.getElementById("popupForm").style.display = "block";
 }
 function closeForm() {
   document.getElementById("popupForm").style.display = "none";
+}
+
+function updateLibrary() {
+  document.getElementById("popupForm").style.display = "none";
+  //   let userName = document.querySelector("#name").value;
+  let bookName = document.querySelector("#bookName").value;
+  let authorName = document.querySelector("#authorName").value;
+  let isRead = document.querySelector("#Yes").value;
+
+  const newBook = new bookCreator(bookName, authorName, isRead);
+
+  listOfBooks.push(newBook);
+  console.log[listOfBooks[0]];
 }
