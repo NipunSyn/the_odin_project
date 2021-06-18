9521be1a29e3f3d7ec6fea15017c4c4011a823f9
Nipun Tewari <nipuntewari11@gmail.com>
2021-06-18 22:42:25 +0530

    pop up forms

--- javascript_projects/to-do list/src/static/js/app.js
+++ javascript_projects/to-do list/src/static/js/app.js
@@ -1,32 +1,30 @@
 // importing body and style sheet
 import "../css/style.css";
+import "../css/form.css";
 import body from "./body";
 body();
 
 // making things interactive
 const addtodo = document.getElementById("addToDo");
 addtodo.addEventListener("click", () => {
-  const ul = document.getElementById("toDoList");
-  const li = document.createElement("li");
-  li.className = "to-do-list";
-  const toDoDiv = document.createElement("div");
-  toDoDiv.className = "to-do-div";
-  toDoDiv.innerHTML = "Hey";
-  li.appendChild(toDoDiv);
-  ul.appendChild(li);
+  document.querySelector(".bg-modal.to-do-modal-container").style.display =
+    "flex";
 });
 
 const addproject = document.getElementById("addProject");
 addproject.addEventListener("click", () => {
-  const ul = document.getElementById("projectList");
-  const li = document.createElement("li");
-  li.classList.add("project");
-  const minidiv = document.createElement("div");
-  minidiv.classList.add("project-div");
-  const p = document.createElement("p");
-  p.classList.add("project-name");
-  p.innerText = "New Project";
-  minidiv.appendChild(p);
-  li.appendChild(minidiv);
-  ul.appendChild(li);
+  document.querySelector(".bg-modal.project-modal-container").style.display =
+    "flex";
+});
+
+const closeProjectBtn = document.getElementById("projectFormClose");
+closeProjectBtn.addEventListener("click", () => {
+  document.querySelector(".bg-modal.project-modal-container").style.display =
+    "none";
+});
+
+const closeToDoBtn = document.getElementById("toDoFormClose");
+closeToDoBtn.addEventListener("click", () => {
+  document.querySelector(".bg-modal.to-do-modal-container").style.display =
+    "none";
 });
