e0ab0e545d6e8c39bf8beda81f2656ea2e4e2a00
Nipun Tewari <nipuntewari11@gmail.com>
2021-07-04 01:32:59 +0530

    caesar cipher test done

--- javascript_projects/testing-practice/js/cipher.js
+++ javascript_projects/testing-practice/js/cipher.js
@@ -1,3 +1,17 @@
-class Cipher {
-    
+//code from Medium- Suyeon Kang
+class CaesarCipher {
+  static encrypt(s, n) {
+    return s
+      .split("")
+      .map((c) => {
+        const ascii = c.charCodeAt(0);
+        if (" " == c) return c;
+        return c.toUpperCase().charCodeAt(0) + n > 90
+          ? String.fromCharCode(ascii + n - 90 + 64)
+          : String.fromCharCode(ascii + n);
+      })
+      .join("");
+  }
 }
+
+module.exports = CaesarCipher
--- javascript_projects/testing-practice/main.test.js
+++ javascript_projects/testing-practice/main.test.js
@@ -1,8 +1,8 @@
 const capitalizeString = require("./js/capitalize-string");
 const reverseString = require("./js/reverse-string");
 const calculator = require("./js/calculator");
-const analyze = require("./js/array-analysis")
-
+const analyze = require("./js/array-analysis");
+const cipher = require("./js/cipher");
 
 //test on capitalize string
 test("capitalize string", () => {
@@ -16,31 +16,33 @@ test("reverse string", () => {
 
 //tests on calculator
 test("add", () => {
-    expect(calculator.add(3, 4)).toBe(7)
-})
+  expect(calculator.add(3, 4)).toBe(7);
+});
 
 test("subtract", () => {
-    expect(calculator.subtract(4, 7)).toBe(-3)
-})
+  expect(calculator.subtract(4, 7)).toBe(-3);
+});
 
 test("multiply", () => {
-    expect(calculator.multiply(23, 3)).toBe(69)
-})
+  expect(calculator.multiply(23, 3)).toBe(69);
+});
 
 test("divide", () => {
-    expect(calculator.divide(7, 0)).toBe(undefined)
-    expect(calculator.divide(15, 3)).toBe(5)
-})
+  expect(calculator.divide(7, 0)).toBe(undefined);
+  expect(calculator.divide(15, 3)).toBe(5);
+});
 
 //tests on caesars-cipher
-
+test("Caesar's cipher", () => {
+  expect(cipher.encrypt("Kfmrk Qbtxof", 3)).toBe("Nipun Tewari");
+});
 
 //tests on array analysis
 test("analyze array", () => {
-    expect(analyze([1, 8, 3, 4, 2, 6])).toEqual({
-      average: 4,
-      min: 1,
-      max: 8,
-      length: 6,
-    });
-})
+  expect(analyze([1, 8, 3, 4, 2, 6])).toEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  });
+});
